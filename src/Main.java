98417f09874dd855e04ed68a7c9810a28963f1c8
Lukas Schauhuber <35468672+luggaesch@users.noreply.github.com>
2021-12-22 10:53:24 +0100

    lösungsvorschlag ergänzt

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
 
-    /*
-        Hier soll die Instanz erstellt werden.
-     */
     public static void main(String[] args) {
+        Dog dog = new Dog("Rex", "German Shepherd", 5);
+        System.out.println(dog);
     }
 }
